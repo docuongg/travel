10dc0c892aac6e19a2b3e1ea0075b7dcee899f4f
Đỗ Ngọc Cường <64196073+docuongg@users.noreply.github.com>
2021-07-10 22:41:24 +0700

    uploads

---
+++ assets/js/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+    $('.slider').owlCarousel({
+        loop:true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    })
+});
+
+$(document).ready(function(){
+    $('.ourteam').owlCarousel({
+        loop:true,
+        margin: 0,
+        autoplay: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            740:{
+                items:2
+            },
+            1024:{
+                items:4
+            }
+        }
+    })
+});
+
+$(document).ready(function(){
+    $('.reviewer__list.owl-carousel').owlCarousel({
+        loop:true,
+        margin: 31,
+        autoplay: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            740:{
+                items:2
+            },
+            1024:{
+                items:3
+            }
+        }
+    })
+});
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin: 20,
+        autoplay: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            740:{
+                items:2
+            },
+            1024:{
+                items:4
+            }
+        }
+    })
+});
+
+// MENU
+
+
+
+
+
+
+  
